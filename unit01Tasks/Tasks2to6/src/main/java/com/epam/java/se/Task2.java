13b2d03bfd007fc3d71dac3284d7dd5019a1af56
Yegor <zakovryash@gmail.com>
2017-02-22 19:33:23 +0300

    Re-do unit01Task2

--- unit01Tasks/Tasks2to6/src/main/java/com/epam/java/se/Task2.java
+++ unit01Tasks/Tasks2to6/src/main/java/com/epam/java/se/Task2.java
@@ -1,14 +1,19 @@
 package com.epam.java.se;
 
 public class Task2 {
+
+    private static final double eps = 1E-4;
+
+    private static double seqValue(int seqElement) {
+        return 1 / Math.pow(seqElement + 1, 2);
+    }
+
     public static void main(String[] args) {
-        double eps = 1E-4;
         int i = 1;
-        double seq = (double) 1/((i+1)*(i+1));
-        while (seq > eps) {
-            System.out.println("Sequence element " + seq + " with number " + i);
+
+        while (seqValue(i) > eps) {
+            System.out.println("Sequence element " + i + " with value " + seqValue(i));
             i++;
-            seq = (double) 1/((i+1)*(i+1));
         }
         System.out.println("Required number " + i);
     }
