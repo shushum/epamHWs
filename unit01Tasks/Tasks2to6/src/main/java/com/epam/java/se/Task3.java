228797944cbe20ac654acd349eba449ba488fb66
Yegor <zakovryash@gmail.com>
2017-02-22 19:40:54 +0300

    Re-do unit01Task3

--- unit01Tasks/Tasks2to6/src/main/java/com/epam/java/se/Task3.java
+++ unit01Tasks/Tasks2to6/src/main/java/com/epam/java/se/Task3.java
@@ -1,18 +1,23 @@
 package com.epam.java.se;
 
 public class Task3 {
+
+    private static final double leftBound = 2;
+    private static final double rightBound = 7;
+    private static final double step = 0.56;
+
+    private static double functionValue(double argument) {
+        return Math.tan(2 * argument) - 3;
+    }
+
     public static void main(String[] args) {
-        double a = 2, b = 7, h = 0.56;
-        double x = a;
-        double F;
-        while (x < b) {
-            F = Math.tan(2*x) - 3;
-            System.out.println("Value of argument: " +  String.format("%(.2f", x) +
-                    "       Value of function: " + F);
-            x+=h;
+        double argument = leftBound;
+        while (argument < rightBound) {
+            System.out.println("Value of argument: " + String.format("%(.2f", argument) +
+                    "       Value of function: " + functionValue(argument));
+            argument += step;
         }
-        F = Math.tan(2*b) - 3;
-        System.out.println("Value of argument: " +  String.format("%(.2f", b) +
-                "       Value of function: " + F);
+        System.out.println("Value of argument: " + String.format("%(.2f", rightBound) +
+                "       Value of function: " + functionValue(rightBound));
     }
 }
