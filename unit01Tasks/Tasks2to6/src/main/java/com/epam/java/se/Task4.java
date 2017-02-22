a2d4cfe32bdeb18bf58e9e612567cd233f97e11a
Yegor <zakovryash@gmail.com>
2017-02-22 19:50:58 +0300

    Re-do unit01Task4

--- unit01Tasks/Tasks2to6/src/main/java/com/epam/java/se/Task4.java
+++ unit01Tasks/Tasks2to6/src/main/java/com/epam/java/se/Task4.java
@@ -4,16 +4,24 @@ package com.epam.java.se;
  * Created by Yegor on 10.02.2017.
  */
 public class Task4 {
-    public static void main(String[] args) {
-        int n = 10;
-        double[] array = new double[n];
-        double max = array[0]+array[n-1];
 
-        for(int i=1; i<n/2; i++){
-            if (max < array[i]+array[n-1-i]){
-                max = array[i]+array[n-1-i];
+    private final double[] array;
+
+    public Task4(double[] array) {
+        this.array = array;
+    }
+
+    private int getLength() {
+        return this.array.length;
+    }
+
+    public double findSpecifiedMax() {
+        double max = Double.MIN_VALUE;
+        for (int i = 0; i < getLength() / 2; i++) {
+            if (max < array[i] + array[getLength() - 1 - i]) {
+                max = array[i] + array[getLength() - 1 - i];
             }
         }
-        System.out.println(max);
+        return max;
     }
 }
