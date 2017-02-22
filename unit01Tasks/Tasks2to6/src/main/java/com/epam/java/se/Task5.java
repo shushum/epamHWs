ad4abe9dcf0944808e70308d7b8bd74814a2560c
Yegor <zakovryash@gmail.com>
2017-02-22 20:04:27 +0300

    Re-do unit01Task5. Add test for it

--- unit01Tasks/Tasks2to6/src/main/java/com/epam/java/se/Task5.java
+++ unit01Tasks/Tasks2to6/src/main/java/com/epam/java/se/Task5.java
@@ -1,20 +1,31 @@
 package com.epam.java.se;
 
 public class Task5 {
-    public static void main(String[] args) {
-        int n = 10;
-        int[][] twoDiag = new int[n][n];
-        for (int i = 0; i<n; i++){
-            for (int j=0; j<n; j++){
-                if (i == j||i == n-1-j) {
-                    twoDiag[i][j] = 1;
+
+    private final int size;
+    private int[][] twoDiagMatrix;
+
+    public Task5(int size){
+        this.size = size;
+        twoDiagMatrix = new int[size][size];
+    }
+
+    private void fillMatrix(){
+        for (int i = 0; i<size; i++){
+            for (int j=0; j<size; j++){
+                if (i == j||i == size-1-j) {
+                    twoDiagMatrix[i][j] = 1;
                 }
             }
         }
+    }
 
-        for (int i = 0; i<n; i++) {
-            for (int j = 0; j < n; j++) {
-                System.out.print(twoDiag[i][j] + "  ");
+    public void printMatrix(){
+        fillMatrix();
+
+        for (int i = 0; i<size; i++) {
+            for (int j = 0; j < size; j++) {
+                System.out.print(twoDiagMatrix[i][j] + "  ");
             }
             System.out.println();
         }
