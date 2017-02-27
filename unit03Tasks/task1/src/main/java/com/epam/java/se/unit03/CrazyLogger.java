b1b3c6b08139c099585d54926de249cc71e414f9
Yegor <zakovryash@gmail.com>
2017-02-27 19:23:39 +0300

    Create 'add' method for CrazyLogger

---
+++ unit03Tasks/task1/src/main/java/com/epam/java/se/unit03/CrazyLogger.java
@@ -0,0 +1,31 @@
+package com.epam.java.se.unit03;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * Created by Yegor on 27.02.2017.
+ */
+public class CrazyLogger {
+    private StringBuilder log;
+    private final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd-MM-YYYY : hh-mm");
+
+
+    public CrazyLogger() {
+        this.log = new StringBuilder();
+    }
+
+    public void addNewMessage(String message){
+        LocalDateTime timeStamp = LocalDateTime.now();
+
+        log.append(timeStamp.format(dateTimeFormatter));
+        log.append(" - ");
+        log.append(message);
+        log.append("\n");
+        log.trimToSize();
+    }
+
+    public String toString(){
+        return log.toString();
+    }
+}
