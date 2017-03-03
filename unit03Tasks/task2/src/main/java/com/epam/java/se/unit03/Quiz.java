5291af009c09f51cc66ad1ee3dc75eb4fbdda62c
Yegor <zakovryash@gmail.com>
2017-03-03 15:20:27 +0300

    Make two methods for listing all questions. Through getKeys() method looks more reliable, though i can't list it in correct order. It will be commented. Gonna use keySet() for now

--- unit03Tasks/task2/src/main/java/com/epam/java/se/unit03/Quiz.java
+++ unit03Tasks/task2/src/main/java/com/epam/java/se/unit03/Quiz.java
@@ -1,26 +1,73 @@
 package com.epam.java.se.unit03;
 
 
-import java.util.Locale;
-import java.util.ResourceBundle;
+import java.util.*;
 
 /**
  * Created by Yegor on 03.03.2017.
  */
 public class Quiz {
+    private ResourceBundle questions;
+    private ResourceBundle answers;
 
-    ResourceBundle questions;
+    public Quiz(Locale chosenLocale) {
+        this.questions = ResourceBundle.getBundle("questions", chosenLocale);
+        this.answers = ResourceBundle.getBundle("answers", chosenLocale);
+    }
 
-    public Quiz(){
-       // Locale locale = new Locale("ru");
-        this.questions = ResourceBundle.getBundle("questions");
+    public String listAllQuestions() {
+        StringBuilder list = new StringBuilder();
+
+        for (int i = 0; i < questions.keySet().size(); i++) {
+            String question = questions.getString("s" + (i + 1));
+            list.append("Question ");
+            list.append(i + 1);
+            list.append(": ");
+            list.append(question);
+            list.append("\n");
+        }
+
+        return list.toString();
+    }
+
+    public String listAllQuestions2() {
+        StringBuilder list = new StringBuilder();
+        Enumeration keys = questions.getKeys();
+
+        while (keys.hasMoreElements()) {
+            String key = (String)keys.nextElement();
+            String question = questions.getString(key);
+
+            list.append(key);
+            list.append(": ");
+            list.append(question);
+            list.append("\n");
+        }
+
+        return list.toString();
+    }
+
+    private void isAcceptableLocale(Locale locale) {
+        Objects.requireNonNull(locale);
+
+        while (true) {
+            if (locale.getLanguage().equals("ru") || locale.getLanguage().equals("en")) {
+                break;
+            }
+            System.out.println("Quiz supports only two versions of language. Please, type 'en' for English or " +
+                    "'ru' for Russian:");
+        }
     }
 
     public static void main(String[] args) {
-        Quiz testQuiz = new Quiz();
+        Locale locale = new Locale("en");
+        Quiz test = new Quiz(locale);
 
 
-            System.out.println(testQuiz.questions.getString("s1"));
+
+        System.out.println(test.listAllQuestions());
+
+
 
     }
 
