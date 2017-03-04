943f690da03ed6cacc4fd527fc2b24924199376b
Yegor <zakovryash@gmail.com>
2017-03-04 19:52:48 +0300

    Change mask for images

--- unit03Tasks/task3/src/main/java/com/epam/java/se/unit03/ImageReferenceScanner.java
+++ unit03Tasks/task3/src/main/java/com/epam/java/se/unit03/ImageReferenceScanner.java
@@ -3,6 +3,8 @@ package com.epam.java.se.unit03;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
@@ -10,33 +12,49 @@ import java.util.regex.Pattern;
  */
 public class ImageReferenceScanner {
     private final static Pattern imageRef =
-            Pattern.compile("(\\([Рр]ис\\. \\d+.*\\))|( [Рр]исун\\D+ \\d+)");
+            Pattern.compile("(\\([Рр]ис\\. ?\\d+((, ?\\d+)?|( и \\d+)?|((-| )[а-я](,[а-я])*)?)\\))|( [Рр]исун\\D\\D \\d+)");
 
     private String fileName;
 
-    public ImageReferenceScanner(String fileName){
+    public ImageReferenceScanner(String fileName) {
         this.fileName = fileName;
     }
 
-    public List<String> readFileBody(){
+    public List<String> readFileBody() {
         List<String> file = new ArrayList<>();
         String current;
 
-        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+        try (BufferedReader reader = new BufferedReader(
+                new InputStreamReader(new FileInputStream(fileName), "windows-1251"))) {
 
             do {
                 current = reader.readLine();
             }
             while (!current.equals("<body>"));
 
-            while ((current = reader.readLine()) != null){
+            while ((current = reader.readLine()) != null) {
                 file.add(current);
             }
-        } catch (IOException e){
+        } catch (IOException e) {
             e.getMessage();
         }
 
         return file;
+    }
+
+    public void imageRefsAreConseq(List<String> file) {
+        Objects.requireNonNull(file);
+        int i = 0;
+        for (String line : file) {
+            Matcher imageRefMatcher = imageRef.matcher(line);
+
+
+            while(imageRefMatcher.find()){
+                System.out.println(i++);
+                System.out.println(imageRefMatcher.group());
+            }
+
+        }
     }
 
     public static void main(String[] args) {
@@ -44,6 +62,6 @@ public class ImageReferenceScanner {
                 "E://Study//java//Projects//unit03Tasks//Java.SE.03.Information handling_task_attachment.html");
 
         List<String> text = test.readFileBody();
-        System.out.println(text.get(0));
+        test.imageRefsAreConseq(text);
     }
 }
