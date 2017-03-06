3cbe95cc369a9269c12e25f1a10b98b08081d22d
Yegor <zakovryash@gmail.com>
2017-03-06 16:00:30 +0300

    Add matchcounter

--- unit04Tasks/task1/src/main/java/com/epam/java/se/unit04/KeyWordsAnalyzer.java
+++ unit04Tasks/task1/src/main/java/com/epam/java/se/unit04/KeyWordsAnalyzer.java
@@ -1,37 +1,41 @@
 package com.epam.java.se.unit04;
 
-import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
-
 import java.io.BufferedInputStream;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by Yegor on 06.03.2017.
  */
 public class KeyWordsAnalyzer {
     private String codeText;
-    private Map<String, Integer> result;
-    private ArrayList<String> keywords = new ArrayList<>();
+    private HashMap<String, Integer> result;
+    private ArrayList<String> keyWords;
 
 
     private void createKeyWordsMapFromFile(String filename) {
-        try (BufferedInputStream inputStream = new BufferedInputStream(new FileInputStream(filename))) {
-            Scanner scanWord = new Scanner(inputStream);
+        Objects.requireNonNull(filename);
 
+        try (BufferedInputStream inputStream = new BufferedInputStream(new FileInputStream(filename))) {
+            ArrayList<String> keywords = new ArrayList<>();
+
+            Scanner scanWord = new Scanner(inputStream);
             while (scanWord.hasNext()) {
                 keywords.add(scanWord.next());
             }
+
+            this.keyWords = keywords;
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public void readFile(String filename) {
+    private void readFile(String filename) {
+        Objects.requireNonNull(filename);
+
         try (BufferedInputStream inputStream = new BufferedInputStream(new FileInputStream(filename))) {
             StringBuilder fileText = new StringBuilder();
             int symbol;
@@ -42,6 +46,28 @@ public class KeyWordsAnalyzer {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public void analyzeFile(String filePath, String keyWordsFilePath) {
+        readFile(filePath);
+        createKeyWordsMapFromFile(keyWordsFilePath);
+        HashMap<String, Integer> result = new HashMap<>();
+
+        for (String keyWord : keyWords) {
+            Pattern keyWordPattern = Pattern.compile(keyWord);
+            Matcher keyWordMatcher = keyWordPattern.matcher(codeText);
+
+            int matchCounter = 0;
+            while (keyWordMatcher.find()) {
+                matchCounter++;
+            }
+
+            if (matchCounter > 0) {
+                result.put(keyWord, matchCounter);
+            }
+        }
+
+        this.result = result;
     }
 
 }
