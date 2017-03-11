5fc1ff20a61143977834a20be5d2a0605835235f
Yegor <zakovryash@gmail.com>
2017-03-11 20:55:33 +0300

    change String to File due to copypast

--- unit05Tasks/task1/src/main/java/com/epam/java/se/unit05/FolderBrowser.java
+++ unit05Tasks/task1/src/main/java/com/epam/java/se/unit05/FolderBrowser.java
@@ -6,19 +6,22 @@ import java.io.File;
  * Created by Yegor on 11.03.2017.
  */
 public class FolderBrowser {
-    private String currentDirectory;
+    private File currentDirectory;
 
 
-    public FolderBrowser(){
-        currentDirectory = System.getProperty("user.home");
+    public FolderBrowser() {
+        currentDirectory = new File(System.getProperty("user.home"));
     }
 
-    public FolderBrowser(String homeDirectory){
-        currentDirectory = homeDirectory;
+    public FolderBrowser(String homeDirectory) {
+        currentDirectory = new File(homeDirectory);
     }
 
-    public void showCurrentDirectory(){
-        File currentDirectory = new File(this.currentDirectory);
-        System.out.println(currentDirectory.getPath() + "> path");
+    public void showCurrentDirectory() {
+        System.out.println(currentDirectory.getPath() + ">");
+    }
+
+    public void showChildrens() {
+
     }
 }
