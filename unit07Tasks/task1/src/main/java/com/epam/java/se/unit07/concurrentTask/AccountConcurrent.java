b2e28102a8a4250ecd67284ba3c236d0f9db58f5
Yegor <zakovryash@gmail.com>
2017-03-18 17:21:40 +0300

    new AccountConcurrent class

---
+++ unit07Tasks/task1/src/main/java/com/epam/java/se/unit07/concurrentTask/AccountConcurrent.java
@@ -0,0 +1,40 @@
+package com.epam.java.se.unit07.concurrentTask;
+
+import com.epam.java.se.unit07.synchronizedTask.Account;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * Created by Yegor on 18.03.2017.
+ */
+public class AccountConcurrent extends Account {
+    private Lock lock = new ReentrantLock(true);
+
+    public AccountConcurrent(String ownerName) {
+        super(ownerName);
+    }
+
+    @Override
+    public void deposit(long value) {
+
+        lock.lock();
+        try {
+            super.deposit(value);
+        } finally {
+            lock.unlock();
+        }
+
+    }
+
+    @Override
+    public void withdraw(long value) {
+
+        lock.lock();
+        try {
+            super.withdraw(value);
+        } finally {
+            lock.unlock();
+        }
+    }
+}
