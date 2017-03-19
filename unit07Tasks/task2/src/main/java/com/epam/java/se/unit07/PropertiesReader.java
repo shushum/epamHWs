81026405f89aa50a8f91d58c4d0a203f53eb373a
Yegor <zakovryash@gmail.com>
2017-03-19 05:09:52 +0300

    Add run with FileChannel and FileLock

--- unit07Tasks/task2/src/main/java/com/epam/java/se/unit07/PropertiesReader.java
+++ unit07Tasks/task2/src/main/java/com/epam/java/se/unit07/PropertiesReader.java
@@ -4,20 +4,23 @@ package com.epam.java.se.unit07;
  * Created by Yegor on 13.03.2017.
  */
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
+import java.nio.channels.FileChannel;
+import java.nio.channels.FileLock;
+import java.nio.channels.OverlappingFileLockException;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Properties;
+import java.util.concurrent.locks.Lock;
 
 /**
  * A class designed to read any .properties file and to get element from it by key.
- *
+ * <p>
  * Each PropertiesReader are bind to the specific .properties file. This is necessary to avoid key duplicating from
  * different .properties files.
  */
-public class PropertiesReader {
-    private String pathToPropertiesFile;
+public class PropertiesReader extends Thread {
+    private final File propertiesFile;
     /**
      * Storage for read properties from .properties file.
      */
@@ -25,28 +28,83 @@ public class PropertiesReader {
 
     /**
      * Creates a PropertiesReader binded to specified .properties file.
-     * @param pathToPropertiesFile String path to .properties file.
+     *
+     * @param propertiesFile .properties file.
      */
-    public PropertiesReader(String pathToPropertiesFile) {
-        this.pathToPropertiesFile = pathToPropertiesFile;
+    public PropertiesReader(File propertiesFile) {
+
+        this.propertiesFile = propertiesFile;
         this.properties = new Properties();
     }
 
     /**
      * Allows to change bindings to another .properties file.
-     * @param newPathToPropertiesFile String path to another .properties file.
+     *
+     * @param newPropertiesFile String path to another .properties file.
      * @return new PropertiesReader binded to another .properties file.
      */
-    public PropertiesReader changePathToPropertiesFile(String newPathToPropertiesFile) {
-        return new PropertiesReader(newPathToPropertiesFile);
+    public PropertiesReader changePathToPropertiesFile(File newPropertiesFile) {
+        return new PropertiesReader(newPropertiesFile);
+    }
+
+    public HashMap<String, String> run(List<String> keys) {
+
+        try {
+            FileChannel channel = new RandomAccessFile(propertiesFile, "r").getChannel();
+
+            FileLock lock = tryLock(channel);
+
+            loadPropertiesFile();
+
+            if (lock != null) /*<--- need?*/ {
+                lock.release();
+            }
+
+            channel.close();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return getElementsByKeys(keys);
+    }
+
+    private FileLock tryLock(FileChannel channel) throws IOException {
+        FileLock lock;
+        
+        try {
+            lock = channel.tryLock();
+        } catch (OverlappingFileLockException e) {
+            throw new OverlappingFileLockException();
+        }
+        return lock;
+    }
+
+    private HashMap<String, String> getElementsByKeys(List<String> keys) {
+        HashMap<String, String> keysWithElements = new HashMap<>();
+
+        for (String key : keys) {
+            try {
+                keysWithElements.put(key, getElementByKey(key));
+            } catch (PropertiesFileIsEmptyException e) {
+                e.printStackTrace();
+            } catch (KeyNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+        return keysWithElements;
     }
 
     /**
      * Loads {@code Properties} from .properties file to PropertiesReader's 'properties' field.
+     *
      * @throws FileNotFoundException if required .properties file does not exist.
      */
-    public void loadPropertiesFile() throws FileNotFoundException {
-        try (BufferedReader reader = new BufferedReader(new FileReader(pathToPropertiesFile))){
+    private void loadPropertiesFile() throws FileNotFoundException {
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(propertiesFile))) {
             properties.load(reader);
         } catch (FileNotFoundException e) {
             throw new FileNotFoundException(e.getMessage());
@@ -57,18 +115,19 @@ public class PropertiesReader {
 
     /**
      * Gets element from .properties file defined by key.
+     *
      * @param key key of required element.
      * @return String element of the key.
      * @throws PropertiesFileIsEmptyException if loaded .properties file is empty.
-     * @throws KeyNotFoundException if such key does not exist in loaded .properties file.
+     * @throws KeyNotFoundException           if such key does not exist in loaded .properties file.
      */
-    public String getElementByKey(String key) throws PropertiesFileIsEmptyException, KeyNotFoundException {
+    private String getElementByKey(String key) throws PropertiesFileIsEmptyException, KeyNotFoundException {
 
-        if (properties.isEmpty()){
+        if (properties.isEmpty()) {
             throw new PropertiesFileIsEmptyException("Properties file is empty.");
         }
 
-        if (properties.getProperty(key) == null){
+        if (properties.getProperty(key) == null) {
             throw new KeyNotFoundException("Such a key does not exist.");
         }
 
