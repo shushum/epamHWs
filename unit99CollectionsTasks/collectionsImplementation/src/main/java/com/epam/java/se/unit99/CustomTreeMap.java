c8a2021b9419acfece5a4973bba955b9de612958
Yegor <zakovryash@gmail.com>
2017-03-30 01:26:31 +0300

    +putAll

--- unit99CollectionsTasks/collectionsImplementation/src/main/java/com/epam/java/se/unit99/CustomTreeMap.java
+++ unit99CollectionsTasks/collectionsImplementation/src/main/java/com/epam/java/se/unit99/CustomTreeMap.java
@@ -71,7 +71,9 @@ public class CustomTreeMap<K extends Comparable<K>, V> implements Map<K, V> {
 
     @Override
     public void putAll(Map<? extends K, ? extends V> m) {
-
+        for (Map.Entry < ? extends K, ? extends V > e: m.entrySet()){
+            put(e.getKey(), e.getValue());
+        }
     }
 
     @Override
