7fd76a71a77d519abfbe2e9349da55ec9c5d0362
Yegor <zakovryash@gmail.com>
2017-04-04 17:47:31 +0300

    javadoc for CustomArrayList

--- unit99CollectionsTasks/collectionsImplementation/src/main/java/com/epam/java/se/unit99/CustomArrayList.java
+++ unit99CollectionsTasks/collectionsImplementation/src/main/java/com/epam/java/se/unit99/CustomArrayList.java
@@ -6,23 +6,48 @@ import java.util.*;
  * Created by Yegor on 03.04.2017.
  */
 
+/**
+ * Custom implementation of ArrayList.
+ * Stores elements of certain type.
+ * This ArrayList allows to store {@code null} values.
+ * @param <T> type of values.
+ */
 public class CustomArrayList<T> implements List<T> {
 
+    /**
+     * Initial size of CustomArrayList.
+     */
     public static final int CAPACITY = 10;
 
+    /**
+     * Array storage for CustomArrayList elements.
+     */
     private Object[] data = new Object[CAPACITY];
+    /**
+     * The size of the CustomArrayList (the number of elements it contains).
+     */
     private int size = 0;
 
+    /**
+     * @return the number of elements in this list
+     */
     @Override
     public int size() {
         return size;
     }
 
+    /**
+     * @return <tt>true</tt> if this list contains no elements.
+     */
     @Override
     public boolean isEmpty() {
         return size == 0;
     }
 
+    /**
+     * @param o element whose presence in this list need to be tested/
+     * @return <tt>true</tt> if this list contains at least one copy of specified element.
+     */
     @Override
     public boolean contains(Object o) {
         for (int i = 0; i < size; i++) {
@@ -33,16 +58,34 @@ public class CustomArrayList<T> implements List<T> {
         return false;
     }
 
+    /**
+     * @return an iterator over the elements of this list (in proper sequence), starting at first element of the list.
+     */
     @Override
     public Iterator<T> iterator() {
         return new ArrayListIterator();
     }
 
+    /**
+     * @return an array containing all of the elements of this list (in proper sequence).
+     */
     @Override
     public Object[] toArray() {
         return Arrays.copyOf(data, size);
     }
 
+    /**
+     * Returns an array containing all of the elements in this list (in proper sequence);
+     * the type of the returned array is that of the specified array.
+     *
+     * If list fits in specified array, it is returned therein. If there is room to spare,
+     * the element in the array immediately following the end of the collection is set to <tt>null</tt>.
+     *
+     * @param a the array into which the elements of list are to be stored, if it is big enough. Otherwise,
+     *          a new array of the same type is used for this purpose.
+     * @param <T1> the type of returned array.
+     * @return an array containing the elements of the list (in proper sequence).
+     */
     @Override
     public <T1> T1[] toArray(T1[] a) {
         if (a.length <= size) {
@@ -54,6 +97,12 @@ public class CustomArrayList<T> implements List<T> {
         }
     }
 
+    /**
+     * Adds the specified element to the end of the list.
+     *
+     * @param t element to be added in this list.
+     * @return <tt>true</tt>
+     */
     @Override
     public boolean add(T t) {
         ensureCapacity();
@@ -61,6 +110,13 @@ public class CustomArrayList<T> implements List<T> {
         return true;
     }
 
+    /**
+     * Removes the first occurrence of the specified element from this list, if it is present.
+     * Otherwise, list is unchanged.
+     *
+     * @param o element to be removed from this list, if it present.
+     * @return <tt>true</tt> if this list contained the specified element.
+     */
     @Override
     public boolean remove(Object o) {
 
@@ -74,6 +130,10 @@ public class CustomArrayList<T> implements List<T> {
         return false;
     }
 
+    /**
+     * @param c collection to be checked for containment in this list.
+     * @return <tt>true</tt> if this list contains all of the elements of the specified collection.
+     */
     @Override
     public boolean containsAll(Collection<?> c) {
         for (Object element : c) {
@@ -84,6 +144,12 @@ public class CustomArrayList<T> implements List<T> {
         return true;
     }
 
+    /**
+     * Adds all of the elements in the specified collection to the end of this list.
+     *
+     * @param c collection containing elements to be added to this list.
+     * @return <tt>true</tt> if this list was changed in result of operation.
+     */
     @Override
     public boolean addAll(Collection<? extends T> c) {
         Objects.requireNonNull(c);
@@ -97,6 +163,16 @@ public class CustomArrayList<T> implements List<T> {
         return size > sizeFlag;
     }
 
+    /**
+     * Adds all of the elements in the specified collection into this
+     * list at the specified position.  Shifts the element currently at that position
+     * (if any) and any subsequent elements to the right.
+     *
+     * @param index index at which to add the first element from the specified collection.
+     * @param c collection containing elements to be added to this list.
+     * @return <tt>true</tt> if this list was changed in result of operation.
+     * @throws IndexOutOfBoundsException if the index is out of range.
+     */
     @Override
     public boolean addAll(int index, Collection<? extends T> c) {
         Objects.requireNonNull(c);
@@ -110,6 +186,13 @@ public class CustomArrayList<T> implements List<T> {
         return size > sizeFlag;
     }
 
+    /**
+     * Removes from this list all of its elements that are contained in the
+     * specified collection.
+     *
+     * @param c collection containing elements to be removed from this list.
+     * @return <tt>true</tt> if this list was changed in result of operation.
+     */
     @Override
     public boolean removeAll(Collection<?> c) {
         Objects.requireNonNull(c);
@@ -123,6 +206,13 @@ public class CustomArrayList<T> implements List<T> {
         return size < sizeFlag;
     }
 
+    /**
+     * Removes from this list all of its elements that are not contained in the
+     * specified collection.
+     *
+     * @param c collection containing elements to be retained in this list.
+     * @return <tt>true</tt> if this list was changed in result of operation.
+     */
     @Override
     public boolean retainAll(Collection<?> c) {
         Objects.requireNonNull(c);
@@ -139,18 +229,36 @@ public class CustomArrayList<T> implements List<T> {
         return size < sizeFlag;
     }
 
+    /**
+     * Removes all of the elements from this list.
+     * The list will be empty after this call returns.
+     */
     @Override
     public void clear() {
         data = new Object[CAPACITY];
         size = 0;
     }
 
+    /**
+     * @param index of the element to return.
+     * @return the element at the specified position of this list.
+     * @throws IndexOutOfBoundsException if the index is out of range.
+     */
     @Override
     public T get(int index) {
         boundsCheck(index);
         return (T) data[index];
     }
 
+    /**
+     * Replaces the element at the specified position in this list with the
+     * specified element.
+     *
+     * @param index index of element to be replaced.
+     * @param element element to be replaced with in specified position.
+     * @return the element that previously was at the specified position.
+     * @throws IndexOutOfBoundsException if the index is out of range.
+     */
     @Override
     public T set(int index, T element) {
         boundsCheck(index);
@@ -160,6 +268,14 @@ public class CustomArrayList<T> implements List<T> {
         return previousValue;
     }
 
+    /**
+     * Adds the element into this list at the specified position.
+     * Shifts the element currently at that position (if any) and any subsequent elements to the right.
+     *
+     * @param index index at which to add the element.
+     * @param element element to be added in list.
+     * @throws IndexOutOfBoundsException if the index is out of range.
+     */
     @Override
     public void add(int index, T element) {
         if (index == size) {
@@ -173,6 +289,15 @@ public class CustomArrayList<T> implements List<T> {
         }
     }
 
+    /**
+     * Removes the element at the specified position in this list.
+     * Shifts any subsequent elements to the left.
+     * Returns the element that was removed from the list.
+     *
+     * @param index the index of the element to be removed.
+     * @return the element that was removed from the list.
+     * @throws IndexOutOfBoundsException if the index is out of range.
+     */
     @Override
     public T remove(int index) {
         boundsCheck(index);
@@ -184,6 +309,13 @@ public class CustomArrayList<T> implements List<T> {
         return value;
     }
 
+    /**
+     * Returns the index of the first occurrence of the specified element
+     * in this list, or -1 if this list does not contain the element.
+     *
+     * @param o the element to search for
+     * @return index of the element.
+     */
     @Override
     public int indexOf(Object o) {
         for (int i = 0; i < size; i++) {
@@ -194,6 +326,13 @@ public class CustomArrayList<T> implements List<T> {
         return -1;
     }
 
+    /**
+     * Returns the index of the last occurrence of the specified element
+     * in this list, or -1 if this list does not contain the element.
+     *
+     * @param o the element to search for
+     * @return index of the element.
+     */
     @Override
     public int lastIndexOf(Object o) {
         for (int i = size - 1; i >= 0; i--) {
@@ -204,16 +343,25 @@ public class CustomArrayList<T> implements List<T> {
         return -1;
     }
 
+    /**
+     * Not implemented.
+     */
     @Override
     public ListIterator<T> listIterator() {
         return null;
     }
 
+    /**
+     * Not implemented.
+     */
     @Override
     public ListIterator<T> listIterator(int index) {
         return null;
     }
 
+    /**
+     * Not implemented.
+     */
     @Override
     public List<T> subList(int fromIndex, int toIndex) {
         return null;
